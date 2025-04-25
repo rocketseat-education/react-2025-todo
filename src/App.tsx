28cac5b30b4009678dcc7e85174a4fa576c17e5a
Vini Fraga <vinifragam@gmail.com>
2025-04-25 15:18:01 -0300

    Estruturando a base - Criando componente Text com CVA

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Text from "./components/text";
+
 export default function App() {
   return (
-    <div className="text-red-500">Olá mundo!</div>
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold" className="text-pink-base">Olá mundo!</Text>
+      <Text className="text-green-base">Olá mundo!</Text>
+      <Text variant="body-md-bold">Olá mundo!</Text>
+      <Text>Levar o dog pra passear</Text>
+    </div>
   )
 }
